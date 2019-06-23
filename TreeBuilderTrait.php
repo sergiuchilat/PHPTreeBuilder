0c05b86f62e9ed351d5492e920f904861583bbd4
usarb <chilatsergiu@gmail.com>
2019-06-23 12:19:15 +0000

    Add files via upload

---
+++ TreeBuilderTrait.php
@@ -0,0 +1,38 @@
+<?
+namespace App;
+
+trait TreeBuilder {
+    /** Построение дерева
+     * @param $flat - сплошной массив
+     * @param $root - ID корневого родительского элемента
+     * @param string $parentField - название родительского поля (по умолчанию parent_id)
+     * @param string $childrenField - поле для сохранения дочерних узлов (по умолчанию children)
+     * @param string $keyField - ключевое поле (по умолчанию id)
+     * @return array - дерево
+     */
+    public function buildTree($flat, $root, $parentField = 'parent_id', $childrenField = 'children', $keyField = 'id') {
+        $parents = [];
+        foreach ($flat as $item) {
+            $parents[$item[$parentField]][] = $item;
+        }
+        return $this->buildBranch($parents, $parents[$root], $childrenField, $keyField);
+    }
+
+    /** Посроение ветки
+     * @param $parents - массив со всеми родителями
+     * @param $children - дети текущей ветки
+     * @param string $childrenField - поле для сохранения дочерних узлов
+     * @param string $keyField - ключевое поле (по умолчанию id)
+     * @return array список узлов текущей ветки
+     */
+    private function buildBranch(&$parents, $children, $childrenField, $keyField) {
+        $tree = array();
+        foreach ($children as $child) {
+            if (isset($parents[$child[$keyField]])) {
+                $child[$childrenField] = $this->buildBranch($parents, $parents[$child[$keyField]], $childrenField, $keyField);
+            }
+            $tree[] = $child;
+        }
+        return $tree;
+    }
+}
